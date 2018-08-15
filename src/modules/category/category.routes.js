d204ec271177cc22decce24401da03ef9ac8fa54
Truong Khoi <khoint@innoteq.vn>
2018-08-15 11:03:23 +0700

    fix some bug at category

--- src/modules/category/category.routes.js
+++ src/modules/category/category.routes.js
@@ -1,13 +1,15 @@
 import { Router } from 'express';
+import validate from 'express-validation';
 import { roleAdmin } from '../../services/role.services';
 import { authJwt } from '../../services/auth.services';
+import categoryValidation from './category.validations';
 import * as categoryController from './category.controllers';
 const routes = new Router();
 
 routes.get('/', categoryController.getCategoryList);
-routes.post('/', authJwt, roleAdmin, categoryController.createCategory);
+routes.post('/', authJwt, roleAdmin, validate(categoryValidation.createCategory), categoryController.createCategory);
 routes.get('/:id', categoryController.getCategory);
-routes.patch('/:id', authJwt, roleAdmin, categoryController.updateCategory);
+routes.patch('/:id', authJwt, roleAdmin, validate(categoryValidation.updateCategory), categoryController.updateCategory);
 routes.delete('/:id', authJwt, roleAdmin, categoryController.deleteCategory);
 
 export default routes;
---
+++ src/modules/category/category.validations.js
@@ -0,0 +1,16 @@
+import Joi from 'joi';
+
+export default {
+    createCategory: {
+        body: {
+            name: Joi.string().min(3).required(),
+            image: Joi.string()
+        }
+    },
+    updateCategory: {
+        body: {
+            name: Joi.string().min(3).required(),
+            image: Joi.string()
+        }
+    }
+};
